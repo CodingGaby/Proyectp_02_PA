8ea59b7176bdea99b746e131f35fe5969d07e6b6
Gaby Osorio <macma.cube1@gmail.com>
2024-03-10 17:58:27 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyectp_02_PA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
